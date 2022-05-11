acbe396dbbcc816b34507e9bc7b089925f6f3402
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-05-11 11:48:31 +0200

    doc(license): add some text at top of each file

--- javascripts/groupmanagement.js
+++ javascripts/groupmanagement.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the YesWiki Extension groupmanagement.
+ *
+ * Authors : see README.md file that was distributed with this source code.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $('#groupManagementEntrySelect #selectedEntry').change(function(){
     $(this).closest('form').submit();
 });
